a701f6dc610005858712c10996949a05b79327c8
jaeyun <wodbs0723@ut.ac.kr>
2024-11-26 23:03:55 +0900

    first commit

---
+++ src/mission01.java
@@ -0,0 +1,24 @@
+/*
+    1. 1~8까지 랜덤한 숫자 배열을 생성
+    2. 교환할 숫자 입력
+    3. 정상적인 입력 들어왔을 경우 턴을 증가시켜 출력.
+    4. 배열이 오름차순이 아닐경우 2부터 다시 반복
+ */
+
+
+import java.util.*;
+
+public class mission01 {
+
+    public static int[] arrMake(){
+        int[] arr = new int[9];
+        for(int i=0;i<9;i++){
+
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
